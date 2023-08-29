17a27e7aff41e3d4f0cec17ab9d6cd0da40155d7
NKIWI0310 <101331037+NKIWI0310@users.noreply.github.com>
2023-08-29 16:34:27 +0900

    Time: 8 ms (98.65%), Space: 43.8 MB (94.61%) - LeetHub

--- 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.java
+++ 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.java
@@ -7,11 +7,11 @@ class Solution {
             prefix[i] = prefix[i - 1] + (customers.charAt(i - 1) == 'N' ? 0 : 1);
         }
 
-        int minDiff = prefix[n]; // 첫 번째 차이는 모두 'N'일 때의 차이입니다.
+        int minDiff = prefix[n]; // 첫 번째 차이는 모두 'N'일 때의 차이
         int bestClosingTime = 0;
 
         for (int i = 1; i <= n; i++) {
-            // 'Y'로 시작하는 문자열의 경우 'Y'의 갯수에 대한 차이와 'N'의 갯수에 대한 차이를 계산합니다.
+            // 'Y'로 시작하는 문자열의 경우 'Y'의 갯수에 대한 차이와 'N'의 갯수에 대한 차이를 계산
             int diff = i - 2 * prefix[i] + prefix[n];
 
             if (diff < minDiff) {
@@ -22,3 +22,25 @@ class Solution {
         return bestClosingTime;
     }
 }
+
+/*
+prefix 는 이전의 값과 모두 N일경우의 차이이다 
+
+YYNY 
+NNNN
+
+고로 
+
+prefix[0] 의 경우 0
+prefix[1] 의 경우 1
+prefix[2] 의 경우 2
+prefix[3] 의 경우 2
+prefix[4] 의 경우 1
+ 
+i시간 전에 도착한 고객 수는 prefix[i]입니다
+i 시간 이후에 도착한 고객 수는 prefix[n] - prefix[i]
+페널티는 i 시간 전에 도착하지 않은 고객의 수와 i 시간 후에 도착한 고객의 수의 합
+따라서 페널티는 (i - prefix[i]) + (prefix[n] - prefix[i]) = i - 2 * prefix[i] + prefix[n]로 계산됨
+int diff = i - 2 * prefix[i] + prefix[n];
+
+ */
