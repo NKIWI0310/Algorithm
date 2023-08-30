0c287a1d7bf40f31d64795645428fb3eae0a52e7
NKIWI0310 <101331037+NKIWI0310@users.noreply.github.com>
2023-08-30 10:46:32 +0900

    Time: 4 ms (86.46%), Space: 54.8 MB (29.17%) - LeetHub

--- 2450-minimum-replacements-to-sort-the-array/2450-minimum-replacements-to-sort-the-array.java
+++ 2450-minimum-replacements-to-sort-the-array/2450-minimum-replacements-to-sort-the-array.java
@@ -1,24 +1,24 @@
 class Solution {
     public long minimumReplacement(int[] nums) {
-        int n = nums.length;       // 배열의 길이
-        int prev = nums[n-1];      // 이전 원소 (끝에서부터 시작)
-        long ans = 0;              // 필요한 연산의 횟수
+        int n = nums.length;
+        int prevElement = nums[n-1];
+        long operations = 0;
         
-        // 끝에서부터 배열의 원소를 확인
         for (int i = n-2; i >= 0; i--) {
-            // 현재 원소를 이전 원소로 몇 번 나눌 수 있는지 계산
-            int noOfTime = nums[i] / prev;
+            if (nums[i] <= prevElement) {
+                prevElement = nums[i];
+                continue;
+            }
             
-            // 나머지가 있을 경우, 원소를 분할해야 함
-            if (nums[i] % prev != 0) {
-                noOfTime++;
-                prev = nums[i] / noOfTime;
-            }   
+            int divisionTimes = nums[i] / prevElement;
+            if (nums[i] % prevElement != 0) {
+                divisionTimes++;
+            }
             
-            // 연산 횟수를 누적
-            ans += noOfTime - 1;
+            operations += divisionTimes - 1;
+            prevElement = nums[i] / divisionTimes;
         }
         
-        return ans;    // 최종 결과 반환
+        return operations;
     }	
 }
