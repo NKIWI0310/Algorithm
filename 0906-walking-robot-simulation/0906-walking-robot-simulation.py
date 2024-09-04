65579e1eb2676a3d1302a5bfa30bd3f2b11e7fbc
NKIWI0310 <101331037+NKIWI0310@users.noreply.github.com>
2024-09-04 15:09:32 +0900

    Time: 248 ms (89.66%), Space: 18.2 MB (29.31%) - LeetHub

---
+++ 0906-walking-robot-simulation/0906-walking-robot-simulation.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def robotSim(self, commands, obstacles):
+        obstacles = set(map(tuple, obstacles))  # Set으로 변환하여 검색 속도 최적화
+        answer = 0
+        x, y = 0, 0
+        cDir = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        cIdx = 0
+
+        for command in commands:
+            if command > 0:  # 이동 처리
+                for _ in range(command):
+                    next_x = x + cDir[cIdx][0]
+                    next_y = y + cDir[cIdx][1]
+                    if (next_x, next_y) not in obstacles:
+                        x, y = next_x, next_y
+                        answer = max(answer, x**2 + y**2)
+                    else:
+                        break
+            else:  # 방향 처리
+                if command == -1:  # 오른쪽 90도 회전
+                    cIdx = (cIdx + 1) % 4
+                else:  # 왼쪽 90도 회전
+                    cIdx = (cIdx - 1) % 4
+
+        return answer
