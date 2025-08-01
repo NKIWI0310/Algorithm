f50f4ff47fe4c1ead882c91b05cb8e5d4e5f961e
NKIWI0310 <101331037+NKIWI0310@users.noreply.github.com>
2025-08-01 11:21:12 +0900

    Time: 0 ms (100%), Space: 12.6 MB (21.66%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def generate(self, numRows):
+        if numRows == 0:
+            return []
+        if numRows == 1:
+            return [[1]]
+        
+        prevRows = self.generate(numRows -1)
+        prevRow = prevRows[-1]
+        currRow = [1]
+
+        for i in range(1, numRows -1):
+            currRow.append(prevRow[i-1] + prevRow[i])
+        
+        currRow.append(1)
+        prevRows.append(currRow)
+
+        return prevRows
+        
