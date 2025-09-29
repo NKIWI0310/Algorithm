36e0008c67b206121acbc8320a493d722df863a6
NKIWI0310 <101331037+NKIWI0310@users.noreply.github.com>
2025-09-30 07:59:08 +0900

    Time: 3724 ms (5.1%), Space: 0B (100%) - LeetHub

--- 1182-game-play-analysis-iv/1182-game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/1182-game-play-analysis-iv.sql
@@ -1,10 +1,17 @@
-/* Write your T-SQL query statement below */
-
-with cte as (
-    select *, lag(event_date) over (partition by player_id order by event_date asc) as prev_date , min(event_date) over (partition by player_id order by event_date asc) as first_login
-    from Activity
+WITH FirstLogin AS (
+    SELECT player_id, MIN(event_date) AS first_login
+    FROM Activity
+    GROUP BY player_id
+),
+NextDayLogin AS (
+    SELECT DISTINCT a.player_id
+    FROM Activity a
+    JOIN FirstLogin f ON a.player_id = f.player_id
+    WHERE DATEDIFF(day, f.first_login, a.event_date) = 1
 )
-
-select round(count(player_id)*1.00 / (select count(distinct player_id) from Activity), 2) as fraction
-from cte
-where prev_date is not null and datediff(day,first_login, event_date) = 1
+SELECT 
+    ROUND(
+        CAST(COUNT(*) AS FLOAT) / (SELECT COUNT(DISTINCT player_id) FROM Activity),
+        2
+    ) AS fraction
+FROM NextDayLogin;
