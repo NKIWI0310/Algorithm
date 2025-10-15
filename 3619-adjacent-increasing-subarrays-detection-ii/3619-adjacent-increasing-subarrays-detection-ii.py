a6b1b941bf2cf37e4f50cf55301a34e48f4db4b4
NKIWI0310 <101331037+NKIWI0310@users.noreply.github.com>
2025-10-15 20:58:58 +0900

    Time: 1767 ms (20%), Space: 38.1 MB (20%) - LeetHub

---
+++ 3619-adjacent-increasing-subarrays-detection-ii/3619-adjacent-increasing-subarrays-detection-ii.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def maxIncreasingSubarrays(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        n = len(nums) #개수만 큼
+        
+        cnt = 1
+        tempCnt = 0
+        ans = 0
+
+        for i in range(1,n):
+            if nums[i] > nums[i-1]: #만약 값이 크다면 계속 증가한다는 의미
+                cnt += 1
+            else:
+                tempCnt, cnt = cnt, 1
+            ans = max(ans, min(cnt, tempCnt)) 
+            ans = max(ans, cnt // 2)
+       
+        return ans
